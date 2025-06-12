fcd7be6d21811cf7d50a1ca5ff3c7edae0084b24
shreyaanjali <anjalishreya880@gmail.com>
2025-06-12 14:35:19 +0530

    added new configuration init first then validate created bucket

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "my-terraform-state-bucket"
-        key = "erraform.tfstate"
+        bucket = "my-terraform-state-bucket50"
+        key = "terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "my-terraform-lock-table"
         encrypt = true
