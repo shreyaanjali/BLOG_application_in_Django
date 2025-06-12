e2f0b65f247efc66107a2985315197ec01ec6db8
shreyaanjali <anjalishreya880@gmail.com>
2025-06-12 14:42:20 +0530

    deleted dynamodb table

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -3,7 +3,6 @@ terraform {
         bucket = "my-terraform-state-bucket50"
         key = "terraform.tfstate"
         region = "us-east-1"
-        dynamodb_table = "my-terraform-lock-table"
         encrypt = true
     }  
 }
