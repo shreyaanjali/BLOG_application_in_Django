3cb55ed90263b630a3f86b52844dec10f215d0f5
shreyaanjali <anjalishreya880@gmail.com>
2025-06-12 14:16:36 +0530

    added new configuration for s3 backend added only backend.tf

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -1,10 +1,9 @@
-
 terraform {
     backend "s3" {
         bucket = "my-terraform-state-bucket"
-        key = "/stateterraform.tfstate"
+        key = "erraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "my-terraform-lock-table"
         encrypt = true
-    }
+    }  
 }
--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -3,12 +3,3 @@ provider "aws" {
 }
 
 
-terraform {
-    backend "s3" {
-        bucket = "my-terraform-state-bucket"
-        key = "state/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "my-terraform-lock-table"
-        encrypt = true
-    }  
-}
